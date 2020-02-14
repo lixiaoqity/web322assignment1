ae364824d03c3226bcdb4404eb6e50c20edddde8
lixiaoqity <59097971+lixiaoqity@users.noreply.github.com>
2020-02-14 15:41:40 -0500

    web assignment 1 change

---
+++ server.js
@@ -0,0 +1,58 @@
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require('body-parser');
+const productModel = require("./modle/product");
+
+const app = express();
+
+app.engine("handlebars",exphbs());
+app.set("view engine","handlebars");
+
+app.use(express.static("public"));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+app.get("/",(req,res)=>{
+
+    res.render("home",{
+        title:"Home",
+        categorySection: productModel.categorySection.getAllCategories(),
+        sellerSection: productModel.products.getAllSellers()
+    });
+
+});
+
+app.get("/products",(req,res)=>{
+
+    res.render("products",{
+        title:"Products",
+        products: productModel.products.getAllProducts()
+    });
+
+});
+
+app.get("/registration",(req,res)=>{
+
+    res.render("registration",{
+        title:"Registration",
+
+    });
+
+});
+
+app.get("/login",(req,res)=>{
+
+    res.render("login",{
+        title:"Login",
+
+    });
+
+});
+
+const PORT =3000;
+app.listen(PORT,()=>{
+    console.log(`Server is succeeded!`)
+})
