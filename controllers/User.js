04127d8fe1765178e77dc9be4ba85c0d7355183f
lixiaoqity <59097971+lixiaoqity@users.noreply.github.com>
2020-04-01 17:56:10 -0400

    change registration part

--- controllers/User.js
+++ controllers/User.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const userModel = require("../models/User");
+//const path=require("path");
 
 router.get("/registration", (req, res) => {
 
@@ -18,9 +19,16 @@ router.get("/dashboard", (req, res) => {
     });
 
 });
-
+/*
 router.post("/registration", (req, res) => {
     console.log(`${req.body.email}`);
+    res.render("User/dashboard");
+});*/
+
+router.post("/registration",(req, res)=> {
+    //console.log(`${req.body.email}`);
+    //console.log(req.body);
+    //res.redirect("/User/dashboard");   
     const errorName = [];
     const errorEmail = [];
     const errorPassword = [];
@@ -34,6 +42,7 @@ router.post("/registration", (req, res) => {
     }
 
     var temp = req.body.password;
+    console.log(req.body.password);
     const regu = RegExp(/^[A-Za-z1-9]+$/);
     if (req.body.password == "") {
         errorPassword.push("Enter your password");
@@ -53,7 +62,7 @@ router.post("/registration", (req, res) => {
         errorPasswordA.push("Password input is inconsistent");
     }
     if (errorName.length > 0 || errorEmail.length > 0 || errorPassword.length > 0 || errorPasswordA.length > 0) {
-        res.render("registration", {
+        res.render("user/registration", {
             title: "Registration",
             errorN: errorName,
             errorE: errorEmail,
@@ -66,36 +75,52 @@ router.post("/registration", (req, res) => {
         });
     }
     else {
-        /*
-        res.render("dashboard", {
-            title: "Dashboard"
-        });*/
-        const {yourName,email}=req.body;
+        //res.redirect("/User/dashboard");
+        //console.log(`${req.body.email}`);
+        
+        const newUser = 
+        {
+            yourName:req.body.yourName,
+            email:req.body.email,
+            password:req.body.password
+        };
+        console.log(newUser.email);
+        
+        const user = new userModel(newUser);
+        
+        //const {yourName,email}=req.body;
         console.log(req.body);
         
         const sgMail = require('@sendgrid/mail');
         sgMail.setApiKey(process.env.WEB322_API_KEY);
         const msg = {
-            to: `${email}`,
-            from: `lixiaoqity@gmail.com`,
+            to: `${newUser.email}`,
+            from: `jessicaguo05@gmail.com`,
             subject: `Welcome to Amazon`,
             html: 
-            `Vistor's Full Name ${yourName} <br>
-            Vistor's Email Address ${email} <br>
+            `Vistor's Full Name ${newUser.yourName} <br>
+            Vistor's Email Address ${newUser.email} <br>
             Welcome to Amazon. Your registration is succeed!<br>
            `,
         };
-        sgMail.send(msg)
+        user.save()
         .then(()=>{
-            res.redirect("/User/dashboard");
+            sgMail.send(msg)
+            .then(()=>{
+                
+                res.render("User/dashboard",{
+                    name:newUser.yourName
+                });
+                //res.redirect("/User/dashboard");
+            })
+            .catch(err=>{
+                console.log(`Error ${err}`);
+            });
+            //res.redirect("/User/dashboard");
         })
-        .catch(err=>{
-            console.log(`Error ${err}`);
-        });
-        
+        .catch(err=>console.log(`Error while inserting into the data ${err}`));
+             
     }
-
-
 });
 
 router.get("/login", (req, res) => {
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
 const express = require("express");
 const exphbs = require("express-handlebars");
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 require('dotenv').config({path:"./config/keys.env"});
 
@@ -13,12 +13,14 @@ const userRoutes = require("./controllers/User");
 
 const app = express();
 
-app.engine("handlebars", exphbs());
-app.set("view engine", "handlebars");
+// parse application/x-www-form-urlencoded
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 //Maps express to all our router objects
 app.use("/",generalRoutes);
@@ -28,6 +30,11 @@ app.use("/",(req,res)=>{
     res.render("General/404");
 });
 
+mongoose.connect(process.env.MONGODB_CONNECTING_WEB322ASSIGNMENT, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log(`Connected to MongoDB Database`);
+})
+.catch(err=>console.log(`Error occured when connecting to database ${err}`));
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Server is succeeded!`);
